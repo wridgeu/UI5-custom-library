779b304e2afb0bf44b18d1f1c1736cde636cccd3
Marco Beier <marco-beier@t-online.de>
2021-01-23 00:17:58 +0100

    Adjusted description and added OpenUI5Support
    (not sure if additional configuration is needed here?)
    
    -> https://sap.github.io/ui5-webcomponents/playground/docs/public-module-imports/ -> #6

--- packages/ui5-library/lib/bundle.js
+++ packages/ui5-library/lib/bundle.js
@@ -1,6 +1,9 @@
 /**
- * Every imported ES6 module in here will get bundled (resvoled, minified etc.)
+ * Every imported ES6 module in here will get bundled (resolved, minified etc.)
  * by rollup.js. For further information check the rollup.config.js file.
+ * For more Icons, add `"@ui5/webcomponents/dist/Assets.js";` (requires bundling adjustment)
+ * or add `import "@ui5/webcomponents-icons/dist/<icon_name>.js";`.
  */
+import "@ui5/webcomponents-base/dist/features/OpenUI5Support.js";
 import "@ui5/webcomponents-icons/dist/employee.js";
 import "@ui5/webcomponents/dist/Avatar.js";
